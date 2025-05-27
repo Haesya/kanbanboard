89abdb7d1674a887171a47de790b039dceeae2c7
Haesya <annamuhanova88@gmail.com>
2025-05-27 20:59:01 +0300

    try to header

---
+++ src/components/header/renderHeader.tsx
@@ -0,0 +1,17 @@
+
+const RenderHeader = () => {
+
+    return (
+        <header>
+            <div>
+                <h1>Awesome Kanban Board</h1>
+            </div>
+            <div>
+                <div>Сначала картинка</div>
+                <button></button>
+            </div>
+        </header>
+    )
+}
+
+export {RenderHeader}
